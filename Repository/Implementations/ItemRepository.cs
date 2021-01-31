609a37b6dff7d5b81d785cb8254e06bc8e32451c
Mihai Draghia <draghia_mihai99@yahoo.com>
2021-01-31 18:27:12 +0200

    get items fro group

--- Repository/Implementations/ItemRepository.cs
+++ Repository/Implementations/ItemRepository.cs
@@ -18,7 +18,7 @@ namespace PetOwner.Repository.Implementations
 		public List<Item> GetItemsByDate(int groupid, DateTime start, DateTime end)
 		{
 
-			return _context.Item.Where(x => x.GroupId == groupid && x.RecordStamp >= start && x.RecordStamp =< end).ToList();
+			return _context.Item.Where(x => x.GroupId == groupid && x.RecordStamp >= start && x.RecordStamp <= end).ToList();
 		}
 	}
 }
